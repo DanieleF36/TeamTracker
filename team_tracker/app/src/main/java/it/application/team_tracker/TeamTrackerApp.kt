08de297f74ff4a453bb0b87100f98399d2257f3d
DanieleF36 <daniele.femia@outlook.com>
2024-07-20 16:39:08 +0200

    removed unused import

--- team_tracker/app/src/main/java/it/application/team_tracker/TeamTrackerApp.kt
+++ team_tracker/app/src/main/java/it/application/team_tracker/TeamTrackerApp.kt
@@ -1,8 +1,6 @@
 package it.application.team_tracker
 
 import android.app.Application
-import androidx.room.Room
 import dagger.hilt.android.HiltAndroidApp
-import it.application.team_tracker.model.localDataSource.database.ApplicationDB
 @HiltAndroidApp
 class TeamTrackerApp: Application()
