880056b7bd8012378830ce6c80eea191a4aff1a0
DanieleF36 <daniele.femia@outlook.com>
2024-07-29 14:42:33 +0200

    added toMap method and modified id to var
    In this way we can push online a new element without the field id

--- team_tracker/app/src/main/java/it/application/team_tracker/model/remoteDataSource/firebase/entities/Entity.kt
+++ team_tracker/app/src/main/java/it/application/team_tracker/model/remoteDataSource/firebase/entities/Entity.kt
@@ -1,3 +1,16 @@
 package it.application.team_tracker.model.remoteDataSource.firebase.entities
 
-interface Entity{val id: String}
+import kotlin.reflect.full.declaredMemberProperties
+
+interface Entity{
+    var id: String
+    fun toMap(): Map<String, Any>{
+        val kClass = this.javaClass.kotlin
+        val ret = emptyMap<String, Any>().toMutableMap()
+        kClass.declaredMemberProperties.forEach {
+            if(it.name != "id")
+                ret[it.name] = it.get(this) as Any
+        }
+        return ret.toMap()
+    }
+}
